fcc6142c2e4f9fecc0bd18c2afe1267d37c34904
sakib213 <sakibhss438@gmail.com>
2022-12-14 01:05:07 +0600

    solved search bar

--- src/Pages/SearchBar.js
+++ src/Pages/SearchBar.js
@@ -4,8 +4,8 @@ import useProducts from '../hooks/useProducts';
 const SearchBar = () => {
     const [products] = useProducts([]);
     return (
-      <div class='py-4  lg:w-96  md:w-96   mx-auto'>
-        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" />
+      <div class='py-4 '>
+        <input type="text" placeholder="Type here" class="input input-bordered input-success w-full block mx-auto max-w-xs" />
 
       </div>
     );
