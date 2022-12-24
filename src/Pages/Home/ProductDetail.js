ab233d9135a0024afc5f1599f58d40de637271dd
sakib213 <sakibhss438@gmail.com>
2022-12-25 02:43:40 +0600

    save code

--- src/Pages/Home/ProductDetail.js
+++ src/Pages/Home/ProductDetail.js
@@ -26,29 +26,29 @@ const ProductDetail = () => {
         }
         ,[])
     return (
-     <div >
+     <div className='mt-4 ' >
            
             {/* <h2>product detail : {product.name}</h2> */}
              <form className='w-24 md:w-80 lg:w-96  flex flex-col mx-auto justify-items-center'   onSubmit={handleSubmit(onSubmit)}>
 
-             <input className='mt-2 h-64 sm:w-24 md:w-80 lg:w-96 rounded ring ring-lime-500' src={img} style={{border:'2px solid red'}} type="image" alt='' {...register("image" )} />
+             <input className='mt-2 h-64  rounded ring ring-lime-500' src={img} style={{border:'2px solid red'}} type="image" alt='' {...register("image" )} />
 
      
-     <textarea className='mt-2  text-lg  sm:w-48 md:w-56  lg:w-96 h-24 rounded' value={name}  style={{border:'2px solid green'}}  {...register("name", { required: true, maxLength: 20 })} />
+     <textarea className='mt-4  text-lg   h-24 rounded' value={name}  style={{border:'2px solid green'}}  {...register("name", { required: true, maxLength: 20 })} />
 
     
-      <input className='mt-2 pl-4  sm:before:w-16 md:w-48 lg:w-48 text-lg text-blue-500 rounded ring ring-green-500'value={price} style={{border:'2px solid red'}} {...register("lastName")} />
+      <input className='mt-2 pl-6 lg:px-16 lg:mx-24 lg:w-48  text-lg text-blue-500 rounded ring ring-green-500'value={price} style={{border:'2px solid red'}} {...register("lastName")} />
       
       
-      
+      <Link to='/checkout'><button class="btn bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... lg:mx-24 lg:w-48 mt-6 text-black">Place Order</button></Link> 
       
     </form>
            
            
                 
         
-         {/* <div className='mt-60'>
-         <Link to='/checkout'><button class="btn btn-accent">Button</button></Link>
+         {/* <div className='w-96 lg:mx-24 mx-auto'>
+    
          </div> */}
      </div>
     );
