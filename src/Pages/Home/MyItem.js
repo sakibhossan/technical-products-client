1cd974e81391212b2bf4b8b8954ae50b53d2d999
sakib213 <sakibhss438@gmail.com>
2023-03-06 16:36:22 +0600

    responsive

--- src/Pages/Home/MyItem.js
+++ src/Pages/Home/MyItem.js
@@ -82,7 +82,7 @@ const MyItem = () => {
         // </div>
         // ------------------------//
           <div className= 'w-96 mx-auto'>
-            <h2 className='ml-32 mt-4 text-lg italic font-medium text-amber-400'>Your Selected Itimes :-</h2>
+            <h2 className=' ml-10 lg:ml-32 mt-4 text-lg italic font-medium text-amber-400'>Your Selected Itimes :-</h2>
             {
                  myItem?.map(order => <ShowMyItem
                     key={order._id}
