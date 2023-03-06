11777ef5a2ca1fb273a310fd6b9a906c3494ebc9
sakib213 <sakibhss438@gmail.com>
2023-03-06 16:20:57 +0600

    small device responsive

--- src/Pages/Home/ShowMyItem.js
+++ src/Pages/Home/ShowMyItem.js
@@ -6,54 +6,7 @@ import { useEffect } from 'react';
 import Loading from '../Shared/Loading';
 
 const ShowMyItem = ({ order ,deleteProduct}) => {
-    // const [user] = useAuthState(auth);
-    const [myItem, setMyItem] = useState([]);
-    // const [loading, setLoading] = useState(false);
-
-
-    // useEffect(() => {
-    //     setLoading(true);
-    //     const getOrders = async () => {
-    //         try {
-    //             const email = user.email;
-    //             const url = `https://technical-backend-code.vercel.app/collectOrder?email=${email}`;
-    //             const config = {
-    //                 headers: {
-    //                     authorization: `Bearer ${localStorage.getItem('acessToken')}`
-    //                 }
-
-    //             }
-    //             const res = await axios.get(url, config);
-    //          setMyItem(res.data);
-    //             setLoading(false);
-    //         } 
-    //         catch (error) {
-    //             console.log(error);
-    //             setLoading(false);
-    //         }
-
-    //     }
-    //     getOrders();
-    // }, [user])
-
-    // const deleteProduct = id => {
-    //     const proceed = window.confirm("Are your sure deliverd this product");
-    //     if (proceed) {
-    //         axios.delete(`https://technical-backend-code.vercel.app/collectOrder/${id}`)
-    //             .then(data => {
-    //                 console.log(data?.data);
-    //                 const remain = myItem?.filter(order => order._id !== id);
-    //                 console.log(remain)
-    //                 setMyItem(remain);
-
-
-    //             })
-    //     }
-
-    // }
-    // if(loading){
-    //     return <Loading></Loading>
-    // }
+   
 
 
     return (
@@ -61,7 +14,7 @@ const ShowMyItem = ({ order ,deleteProduct}) => {
 
 
 
-        <div class="card w-96 bg-base-200 mt-4 text-primary-content">
+        <div class="card  lg:w-96 sm:w-80 bg-base-200 mt-4 text-primary-content">
             <div class="card-body">
                 <h2 class="card-title">{order.product}</h2>
                 <h3 className='mx-auto'><span className='text-blue-500'>Price: $</span>{order.price}</h3>
