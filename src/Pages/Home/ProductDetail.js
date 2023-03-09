f5c22a7b74b66da376e80209ccba578e84159d0d
sakib213 <sakibhss438@gmail.com>
2023-03-09 18:28:23 +0600

    like this css

--- src/Pages/Home/ProductDetail.js
+++ src/Pages/Home/ProductDetail.js
@@ -20,13 +20,13 @@ const ProductDetail = () => {
 
             <form className=' w-72 lg:w-96   mx-auto  mt-4' onSubmit={handleSubmit(onSubmit)}>
 
-                <input className= 'w-80 lg:w-96 mt-2   rounded ring ring-lime-500' src={img} style={{ border: '2px solid red' }} type="image" alt='' {...register("image")} />
+                <input className= 'w-72 lg:w-96  mt-2   rounded ring ring-lime-500' src={img} style={{ border: '2px solid red' }} type="image" alt='' {...register("image")} />
 
 
                 <textarea className='w-56 lg:w-72 px-10 mx-10 mt-4 text-lg  lg:text-xl  py-2    rounded' value={name} style={{ border: '2px solid green' }}  {...register("name", { required: true, maxLength: 20 })} />
 
 
-                <input className='w-32 mx-24 mt-2 px-12  text-lg text-blue-500 rounded ring ring-green-500' value={price} style={{ border: '2px solid red' }} {...register("lastName")} />
+                <input className='w-32 mx-24 mt-2 px-10  text-lg text-blue-500 rounded ring ring-green-500' value={price} style={{ border: '2px solid red' }} {...register("lastName")} />
 
                 <br />
                 <Link to={`/checkout/${productId}`}><button class="btn bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... w-32 mx-24   mt-6 text-black">Place Order</button></Link>
